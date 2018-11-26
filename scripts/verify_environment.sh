c402c82aa55464d279cf357eb3fc533156f98aaa
vidir17 <vidir17@ru.is>
2018-11-26 18:16:56 +0000

    comments on script

--- scripts/verify_environment.sh
+++ scripts/verify_environment.sh
@@ -1,14 +1,23 @@
 #!/bin/sh
-#Message display and current date and time
+#Message display and current date and time at start of script
 printf "Script starts at: " && date | tee logfile.log
 #Message display and who the current user is
 printf "Greetings " && whoami | tee -a logfile.log
-printf "This script prompts the user with generic information about the system\n" #Message display
-printf "*****Current version*****\n" #Message display
+#Message display
+printf "This script prompts the user with generic information about the system\n" 
+#Message display
+printf "*****Current version*****\n" 
+#Shows current version of linux
 lsb_release -a | tee -a logfile.log
+#Shows current version of git
 git --version | tee -a logfile.log
+#For readability in log file
 printf "npm version " >> logfile.log
+#Shows current version of npm
 printf "npm version " && npm --version | tee -a logfile.log
+#For readability in log file
 printf "nodejs version " >> logfile.log
+#Shows current version of nodejs
 printf "nodejs version " && nodejs --version | tee -a logfile.log
+#Message display and current date and time at end of script
 printf "Script ends at: " && date | tee -a logfile.log
