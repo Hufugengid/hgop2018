6d2b9d95463346fdc4d300428401a50cc4643457
vidir17 <vidir17@ru.is>
2018-11-28 22:32:47 +0000

    if else statements for npm, nodejs and aws if not installed it shows error message

--- scripts/verify_environment.sh
+++ scripts/verify_environment.sh
@@ -11,17 +11,36 @@ printf "*****Current version*****\n"
 lsb_release -a | tee -a logfile.log
 #Shows current version of git
 git --version | tee -a logfile.log
+#if ! [ -x "$(command -v jest)" ]; then
+#  echo 'Error: jest is not installed.' 
+#fi
+if ! [ -x "$(command -v npm)" ]; then
+  echo 'Error: npm is not installed.' 
+else
 #For readability in log file
 printf "npm version " >> logfile.log
 #Shows current version of npm
 printf "npm version " && npm --version | tee -a logfile.log
 #For readability in log file
+
+fi
+
+if ! [ -x "$(command -v node)" ]; then
+  echo 'Error: nodejs is not installed.' 
+else
 printf "nodejs version " >> logfile.log
 #Shows current version of nodejs
 printf "nodejs version " && nodejs --version | tee -a logfile.log
+fi
+
+if ! [ -x "$(command -v aws)" ]; then
+  echo 'Error: aws is not installed.' 
+else
 #For readability in log file
 printf "aws version " >> logfile.log
 #Shows current version of aws
 printf "aws version " && aws --version | tee -a logfile.log
+fi
+
 #Message display and current date and time at end of script
 printf "Script ends at: " && date | tee -a logfile.log
