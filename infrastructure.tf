45f08ca6ecd7843329c94a74db98342589c05cc3
vidir17 <vidir17@ru.is>
2018-11-29 15:00:46 +0000

    Commenting the terraform file

--- infrastructure.tf
+++ infrastructure.tf
@@ -1,10 +1,12 @@
-# TODO Comment 2-3 sentences.
+# This tells us that we want to use aws cloud service as a provider.
+# The cloud services region is us-east-1.
 provider "aws" {
   shared_credentials_file = "~/.aws/credentials"
   region                  = "us-east-1"
 }
 
-# TODO Comment 2-3 sentences.
+# Creates a security group called game and uses the public port 22 and 3000, egress is outbound traffic for any.
+
 resource "aws_security_group" "game_security_group" {
   name   = "GameSecurityGroup"
 
@@ -29,8 +31,8 @@ resource "aws_security_group" "game_security_group" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
-
-# TODO Comment 2-3 sentences.
+# Creating aws instance for game 
+# going trough the following steps, naming and specifying the values below.
 resource "aws_instance" "game_server" {
   ami                    = "ami-0ac019f4fcb7cb7e6"
   instance_type          = "t2.micro"
@@ -39,7 +41,7 @@ resource "aws_instance" "game_server" {
   tags {
     Name = "GameServer"
   }
-  # TODO Comment 1-2 sentences.
+  # Gets the api script and puts it in the desired destination. Tells what connection is needed and where the key is located.
   provisioner "file" {
     source      = "scripts/initialize_game_api_instance.sh"
     destination = "/home/ubuntu/initialize_game_api_instance.sh"
@@ -50,7 +52,7 @@ resource "aws_instance" "game_server" {
       private_key = "${file("~/.aws/GameKeyPair.pem")}"
     }
   }
-  # TODO Comment 1-2 sentences.
+  # Gets the docker file and puts it in the desired destination. Tells what connection is needed and where the key is located.
   provisioner "file" {
     source      = "docker-compose.yml"
     destination = "/home/ubuntu/docker-compose.yml"
@@ -66,7 +68,8 @@ resource "aws_instance" "game_server" {
   # Since it can take time for the SSH agent on machine to start up we let Terraform
   # handle the retry logic, it will try to connect to the agent until it is available
   # that way we know the instance is available through SSH after Terraform finishes.
-  # TODO Comment 1-2 sentences.
+  
+  # Makes the api script remote executable.
   provisioner "remote-exec" {
     inline = [
       "chmod +x /home/ubuntu/initialize_game_api_instance.sh",
@@ -80,7 +83,7 @@ resource "aws_instance" "game_server" {
   }
 }
 
-# TODO Comment 1-2 sentences.
+# Giving the variable public_ip the ip on the server.
 output "public_ip" {
   value = "${aws_instance.game_server.public_ip}"
 }
