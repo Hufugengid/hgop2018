299e05edb67ef21a92813d663991b77c0a122c57
jakobj13 <jakobj13@ru.is>
2018-12-11 16:21:21 +0000

    server lib test changed a bit

--- scripts/sync_session.sh
+++ scripts/sync_session.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-JENKINS_URL=ec2-54-80-8-10.compute-1.amazonaws.com
+JENKINS_URL=ec2-3-81-16-174.compute-1.amazonaws.com
 
-scp -o StrictHostKeyChecking=no -i "~/.aws/JenkinsAWSKeyPair.pem" ~/.aws/credentials ubuntu@${ec2-54-80-8-10.compute-1.amazonaws.com}:~/credentials
-ssh -o StrictHostKeyChecking=no -i "~/.aws/JenkinsAWSKeyPair.pem" ubuntu@${ec2-54-80-8-10.compute-1.amazonaws.com} "sudo mv ~/credentials /var/lib/jenkins/.aws/credentials"
-ssh -o StrictHostKeyChecking=no -i "~/.aws/JenkinsAWSKeyPair.pem" ubuntu@${ec2-54-80-8-10.compute-1.amazonaws.com} "sudo chmod a+r /var/lib/jenkins/.aws/credentials"
+scp -o StrictHostKeyChecking=no -i "~/.aws/JenkinsAWSKeyPair.pem" ~/.aws/credentials ubuntu@${JENKINS_URL}:~/credentials
+ssh -o StrictHostKeyChecking=no -i "~/.aws/JenkinsAWSKeyPair.pem" ubuntu@${JENKINS_URL} "sudo mv ~/credentials /var/lib/jenkins/.aws/credentials"
+ssh -o StrictHostKeyChecking=no -i "~/.aws/JenkinsAWSKeyPair.pem" ubuntu@${JENKINS_URL} "sudo chmod a+r /var/lib/jenkins/.aws/credentials"
 
