036872ef1030f0126965df1064f0a2bc79a9203e
jakobj13 <jakobj13@ru.is>
2018-12-12 22:28:33 +0000

    Docker tags removed

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -3,9 +3,9 @@
 GIT_COMMIT=$1
 
 cd game-api
-docker build -t jakobj13/hgop:game-api$GIT_COMMIT . || exit 1
+docker build -t jakobj13/hgop:$GIT_COMMIT . || exit 1
 
 cd ../game-client
-docker build -t jakobj13/hgop:uiTag$GIT_COMMIT . || exit 1
+docker build -t jakobj13/UI:$GIT_COMMIT . || exit 1
 
 # TODO exit on error if any command fails
