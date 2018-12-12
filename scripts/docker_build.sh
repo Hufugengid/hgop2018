380a970587f139b7be2df34426026534b3c25344
jakobj13 <jakobj13@ru.is>
2018-12-12 22:46:18 +0000

    ui to lowercase

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -6,6 +6,6 @@ cd game-api
 docker build -t jakobj13/hgop:$GIT_COMMIT . || exit 1
 
 cd ../game-client
-docker build -t jakobj13/UI:$GIT_COMMIT . || exit 1
+docker build -t jakobj13/ui:$GIT_COMMIT . || exit 1
 
 # TODO exit on error if any command fails
--- scripts/docker_push.sh
+++ scripts/docker_push.sh
@@ -5,6 +5,6 @@ GIT_COMMIT=$1
 docker push jakobj13/hgop:$GIT_COMMIT || exit 1
 
 
-docker push jakobj13/UI:$GIT_COMMIT || exit 1
+docker push jakobj13/ui:$GIT_COMMIT || exit 1
 
 # TODO exit on error if any command fails
