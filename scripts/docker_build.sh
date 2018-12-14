30e98d46ef8300e9d5dc5f446e0f11e52bb2f809
jakobj13 <jakobj13@ru.is>
2018-12-14 15:33:03 +0000

    docker tag changed

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -3,8 +3,8 @@
 GIT_COMMIT=$1
 
 cd game-api
-docker build -t jakobj13/hgop:game_$GIT_COMMIT . || exit 1
+docker build -t jakobj13/hgop:$GIT_COMMIT . || exit 1
 
 cd ..
 cd game-client
-docker build -t jakobj13/hgop:ui_$GIT_COMMIT . || exit 1
+docker build -t jakobj13/ui:$GIT_COMMIT . || exit 1
--- scripts/docker_push.sh
+++ scripts/docker_push.sh
@@ -2,9 +2,9 @@
 
 GIT_COMMIT=$1
 
-docker push jakobj13/hgop:game_$GIT_COMMIT || exit 1
+docker push jakobj13/hgop:$GIT_COMMIT || exit 1
 
 
-docker push jakobj13/ui:_$GIT_COMMIT || exit 1
+docker push jakobj13/ui:$GIT_COMMIT || exit 1
 
 # TODO exit on error if any command fails
